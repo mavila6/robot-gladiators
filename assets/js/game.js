170f4facb344495b13cf5be1324a6942e55ac7d8
Marco Avila <mavila6@email.arizona.com>
2021-05-24 13:03:22 -0700

    added console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,10 @@
 var playerName = window.prompt("What is your robot's name?");
 
+console.log(playerName);
+console.log("Our robot's name is " + playerName);
+
 function fight() {
     window.alert("The fight has begun!");
 }
 
-fight();
+//fight();
